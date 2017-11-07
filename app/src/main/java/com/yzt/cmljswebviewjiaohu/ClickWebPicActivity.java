a23541ea11f694b42fd0840323592094ca68d02b
chenmingliang <229473390@qq.com>
2017-11-07 14:40:05 +0800

    恢复误删的demo.html 解决clickPic无法响应的问题

--- app/src/main/java/com/yzt/cmljswebviewjiaohu/ClickWebPicActivity.java
+++ app/src/main/java/com/yzt/cmljswebviewjiaohu/ClickWebPicActivity.java
@@ -7,23 +7,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class ClickWebPicActivity extends AppCompatActivity {
 
     private WebView mWebView;
     String url = "https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_15015129107283868386%22%7D&n_type=0&p_from=1";
+    private SeekBar mSeekBar;
+
     @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_web_pic);
 
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
         mWebView = (WebView) findViewById(R.id.webview);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                mSeekBar.setProgress(newProgress);
+            }
+        });
 
         mWebView.loadUrl(url);
 
@@ -40,9 +53,9 @@ public class ClickWebPicActivity extends AppCompatActivity {
             this.context = context;
         }
 
+        @android.webkit.JavascriptInterface //这个注解必须加上，否则无效
         public void openImage(String img){
-            Log.e("CML","openImage");
-            Toast.makeText(context, ""+img, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "pic的图片地址："+img, Toast.LENGTH_LONG).show();
         }
     }
 
@@ -52,20 +65,17 @@ public class ClickWebPicActivity extends AppCompatActivity {
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            Log.e("CML","onPageFinished");
             addImageClickListener();
         }
 
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            Log.e("CML","onPageStarted");
             view.getSettings().setJavaScriptEnabled(true);
             super.onPageStarted(view, url, favicon);
         }
     }
 
     private void addImageClickListener() {
-        Log.e("CML","addImageClickListener");
         mWebView.loadUrl("javascript:(function(){" +
                 "var objs = document.getElementsByTagName(\"img\"); " +
                 "for(var i=0;i<objs.length;i++)  " +
